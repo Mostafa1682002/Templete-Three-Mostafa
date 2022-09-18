52a60aad5762c3445e6bdbd7330c8eec04af0e5b
Mostafa1682002 <ma9856603@gmail.com>
2022-09-18 22:36:02 +0200

    Rev

--- js/main.js
+++ js/main.js
@@ -26,6 +26,23 @@ allLinks.forEach((e)=>{
 })
 
 
+//Gallery  Imgs Click
+let allBoxGallery=document.querySelectorAll('.box-gallery');
+let overlayGallary=document.querySelector('.overlay-gallary');
+let currentImgGallary=document.querySelector('.over-img');
+let closeOverlay=document.querySelector('.close-overlay');
+allBoxGallery.forEach((box)=>{
+  box.addEventListener('click',()=>{
+    overlayGallary.classList.add('active');
+    currentImgGallary.src=box.firstElementChild.firstElementChild.src;
+  })
+})
+
+closeOverlay.addEventListener('click',()=>{
+  overlayGallary.classList.remove('active');
+  currentImgGallary.src=''
+})
+
 
 
 
@@ -33,88 +50,64 @@ allLinks.forEach((e)=>{
 // Animate Width On Scrolling
 let skillsSection=document.querySelector('.skills');
 let allprogress=document.querySelectorAll('.prog');
+
+
+//Counter On Scrolling
+let statsSection=document.querySelector(".stats");
+let allNumberCount=document.querySelectorAll('.box-stats h3');
+
+function startCount(ele){
+  let counter=setInterval(()=>{
+      if(ele.textContent==ele.getAttribute('data-number')){
+        clearInterval(counter);
+      }else{
+        ele.textContent++;
+      }
+  },2000/ele.getAttribute('data-number'));
+}
+
 window.onscroll=function(){
-  if(window.scrollY>=skillsSection.offsetTop-200){
+  if(window.scrollY>=skillsSection.offsetTop-300){
     allprogress.forEach((e)=>{
       e.style.width=`${e.getAttribute('data-prog')}%`;
     })
   }
+
+  if(window.scrollY>=statsSection.offsetTop-200){
+    allNumberCount.forEach((e)=>startCount(e))
+  }
 }
 
-// /*
-//  ** Countdown Timer
-//  ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
-//  */
 
-// // The End Of The Year Date
-// // 1000 milliseconds = 1 Second
 
-// let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
-// // console.log(countDownDate);
+//LATEST EVENTS 
+let daysHtml=document.querySelector('.days')
+let hoursHtml=document.querySelector('.hours')
+let munitseHtml=document.querySelector('.munites')
+let secondsHtml=document.querySelector('.seconds')
 
-// let counter = setInterval(() => {
-//   // Get Date Now
-//   let dateNow = new Date().getTime();
+let dateNow=new Date();
+let countDown=setInterval(()=>{
+  let dateEvents=new Date('Dec 31, 2023 23:59:59');
+  let dif=dateEvents.getTime()-dateNow.getTime();
+  if(dif>0){
+    let days=Math.floor(dif/1000/60/60/24);
+    let hours=Math.floor((dif%(1000*60*60*24))/(1000*60*60));
+    let munites=Math.floor((dif%(1000*60*60))/(1000*60));
+    let seconds=Math.floor((dif%(1000*60))/(1000));
+    daysHtml.innerHTML=days>9?days:'0'+days;
+    hoursHtml.innerHTML=hours>9?hours:'0'+hours;
+    munitseHtml.innerHTML=munites>9?munites:'0'+munites;
+    secondsHtml.innerHTML=seconds>9?seconds:'0'+seconds;
+  }else{
+    daysHtml.innerHTML='00';
+    hoursHtml.innerHTML='00';
+    munitseHtml.innerHTML='00';
+    secondsHtml.innerHTML='00';    
+      clearInterval(countDown);
+  }
+},1000);
 
-//   // Find The Date Difference Between Now And Countdown Date
-//   let dateDiff = countDownDate - dateNow;
 
-//   // Get Time Units
-//   // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-//   let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-//   let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-//   let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-
-//   document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-//   document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-//   document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-//   document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-//   if (dateDiff < 0) {
-//     clearInterval(counter);
-//   }
-// }, 1000);
-
-// /*
-//  ** Animate Width On Scrolling
-//  ** Video URL: https://youtu.be/sbIoIKI9FOc
-//  */
-
-// /*
-//  ** Increase Numbers On Scrolling
-//  ** Video URL: https://youtu.be/PLsUdgLnzgQ
-//  */
-
-// let progressSpans = document.querySelectorAll(".the-progress span");
-// let section = document.querySelector(".our-skills");
-
-// let nums = document.querySelectorAll(".stats .number");
-// let statsSection = document.querySelector(".stats");
-// let started = false; // Function Started ? No
-
-// window.onscroll = function () {
-//   // Skills Animate Width
-//   if (window.scrollY >= section.offsetTop - 250) {
-//     progressSpans.forEach((span) => {
-//       span.style.width = span.dataset.width;
-//     });
-//   }
-//   // Stats Increase Number
-//   if (window.scrollY >= statsSection.offsetTop) {
-//     if (!started) {
-//       nums.forEach((num) => startCount(num));
-//     }
-//     started = true;
-//   }
-// };
-
-// function startCount(el) {
-//   let goal = el.dataset.goal;
-//   let count = setInterval(() => {
-//     el.textContent++;
-//     if (el.textContent == goal) {
-//       clearInterval(count);
-//     }
-//   }, 2000 / goal);
-// }
+let copyYear=document.querySelector('.year');
+copyYear.innerHTML=dateNow.getFullYear();
